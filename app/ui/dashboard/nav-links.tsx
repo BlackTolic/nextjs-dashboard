146cd36cf7e8c9a667a83de204992400903ae2c2
月牙魔法师 <617938514@qq.com>
2024-12-26 23:00:01 +0800

    p5.Link组件优点，usePathname,link active

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,8 +1,23 @@
+// 为了调用usePathname，因为usePathname 是个hook函数
+'use client';
+
+
 import {
   UserGroupIcon,
   HomeIcon,
   DocumentDuplicateIcon,
 } from '@heroicons/react/24/outline';
+
+// To link between pages, you'd traditionally use the <a> HTML element.
+// At the moment, the sidebar links use <a> elements, but notice what happens when you
+// navigate between the home, invoices, and customers pages on your browser.
+// There's a full page refresh on each page navigation!
+import Link from 'next/link';
+// 获取当前
+import { usePathname } from 'next/navigation';
+
+import clsx from 'clsx';
+
 
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
@@ -17,19 +32,26 @@ const links = [
 ];
 
 export default function NavLinks() {
+  const pathname = usePathname();
+
   return (
     <>
       {links.map((link) => {
         const LinkIcon = link.icon;
         return (
-          <a
+          <Link
             key={link.name}
             href={link.href}
-            className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
+            className={clsx(
+                'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
+                {
+                  'bg-sky-100 text-blue-600': pathname === link.href,
+                },
+            )}
           >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
-          </a>
+          </Link>
         );
       })}
     </>
