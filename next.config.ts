1e60295262d2fbf01153fdf1b832fa142de61484
月牙魔法师 <617938514@qq.com>
2024-12-30 17:26:54 +0800

    10.Partial Prerendering

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+    // 开启Partial Prerendering
+    // experimental: {
+    //     ppr: 'incremental',
+    // },
 };
 
 export default nextConfig;
