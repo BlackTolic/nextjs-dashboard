04003c5dbb44b72b5e3ced9653e33678aada6aa5
月牙魔法师 <617938514@qq.com>
2025-01-02 15:21:31 +0800

    12.新增编辑删除功能，使用动态路由段控制页面

---
+++ app/lib/actions.ts
@@ -0,0 +1,69 @@
+'use server';
+
+import { z } from 'zod';
+import { sql } from '@vercel/postgres';
+
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+
+const FormSchema = z.object({
+    id: z.string(),
+    customerId: z.string(),
+    // coerce强制转换类型
+    amount: z.coerce.number(),
+    status: z.enum(['pending', 'paid']),
+    date: z.string(),
+});
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
+export async function createInvoice(formData: FormData) {
+    console.log(formData,'formData');
+    // 将form输入的值进行格式转换和校验
+    const { customerId, amount, status } = CreateInvoice.parse({
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+    });
+    const amountInCents = amount * 100;
+    const date = new Date().toISOString().split('T')[0];
+    // Test it out:
+    console.log( customerId, amount, status,' customerId, amount, status');
+    // 写入数据库
+    await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
+    // 这个可以清除客户端的路由段缓存
+    // Once the database has been updated,this path will be revalidated
+    // revalidatePath('/dashboard/invoices');
+    // 跳回列表
+    redirect('/dashboard/invoices');
+
+}
+
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+
+export async function updateInvoice(id: string, formData: FormData) {
+    const { customerId, amount, status } = UpdateInvoice.parse({
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+    });
+
+    const amountInCents = amount * 100;
+
+    await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
+}
+
+export async function deleteInvoice(id: string) {
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+    revalidatePath('/dashboard/invoices');
+}
