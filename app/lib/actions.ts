95fe534d3c0b7906e899f79a90131272f6fa30b1
月牙魔法师 <617938514@qq.com>
2025-01-02 20:08:26 +0800

    14.client表单校验,前端校验require，后端校验validate

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,84 +1,113 @@
-'use server';
+"use server";
 
-import { z } from 'zod';
-import { sql } from '@vercel/postgres';
+import { z } from "zod";
+import { sql } from "@vercel/postgres";
 
-import { revalidatePath } from 'next/cache';
-import { redirect } from 'next/navigation';
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 
 const FormSchema = z.object({
-    id: z.string(),
-    customerId: z.string(),
-    // coerce强制转换类型
-    amount: z.coerce.number(),
-    status: z.enum(['pending', 'paid']),
-    date: z.string(),
+  id: z.string(),
+  customerId: z.string({ invalid_type_error: "Please select a customer." }),
+  // coerce强制转换类型
+  amount: z.coerce
+    .number()
+    .gt(0, { message: "Please enter an amount greater than $0." }),
+  status: z.enum(["pending", "paid"], {
+    invalid_type_error: "Please select an invoice status.",
+  }),
+  date: z.string(),
 });
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+};
+// prevState - contains the state passed from the useActionState hook.
+// You won't be using it in the action in this example, but it's a required prop.
+export async function createInvoice(prevState: State, formData: FormData) {
+  // console.log(formData, "formData");
+  // 将form输入的值进行格式转换和校验
+  // const { customerId, amount, status } = CreateInvoice.parse({
 
-export async function createInvoice(formData: FormData) {
-    console.log(formData,'formData');
-    // 将form输入的值进行格式转换和校验
-    const { customerId, amount, status } = CreateInvoice.parse({
-        customerId: formData.get('customerId'),
-        amount: formData.get('amount'),
-        status: formData.get('status'),
-    });
-    const amountInCents = amount * 100;
-    const date = new Date().toISOString().split('T')[0];
-    // Test it out:
-    console.log( customerId, amount, status,' customerId, amount, status');
-    try {
+  // safeParse() will return an object containing either a success or error field.
+  // This will help handle validation more gracefully without having put this logic inside the try/catch block.
+  const validatedFields = CreateInvoice.safeParse({
+    customerId: formData.get("customerId"),
+    amount: formData.get("amount"),
+    status: formData.get("status"),
+  });
+
+  console.log(validatedFields, "validatedFields");
+
+  // If form validation fails, return errors early. Otherwise, continue.
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: "Missing Fields. Failed to Create Invoice.",
+    };
+  }
+
+  // Prepare data for insertion into the database
+  const { customerId, amount, status } = validatedFields.data;
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split("T")[0];
+
+  // Test it out:
+  // console.log(customerId, amount, status, " customerId, amount, status");
+  try {
     // 写入数据库
     await sql`
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
-    } catch (error) {
-        return {
-            message: 'Database Error: Failed to Create Invoice.',
-        };
-    }
-    // 这个可以清除客户端的路由段缓存
-    // Once the database has been updated,this path will be revalidated
-    // revalidatePath('/dashboard/invoices');
-    // 跳回列表
-    redirect('/dashboard/invoices');
-
+  } catch (error) {
+    return {
+      message: "Database Error: Failed to Create Invoice.",
+    };
+  }
+  // 这个可以清除客户端的路由段缓存
+  // Once the database has been updated,this path will be revalidated
+  revalidatePath("/dashboard/invoices");
+  // 跳回列表
+  redirect("/dashboard/invoices");
 }
 
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
 export async function updateInvoice(id: string, formData: FormData) {
-    const { customerId, amount, status } = UpdateInvoice.parse({
-        customerId: formData.get('customerId'),
-        amount: formData.get('amount'),
-        status: formData.get('status'),
-    });
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get("customerId"),
+    amount: formData.get("amount"),
+    status: formData.get("status"),
+  });
 
-    const amountInCents = amount * 100;
-    try {
+  const amountInCents = amount * 100;
+  try {
     await sql`
     UPDATE invoices
     SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
     WHERE id = ${id}
   `;
-    } catch (error) {
-        return { message: 'Database Error: Failed to Update Invoice.' };
-    }
-    revalidatePath('/dashboard/invoices');
-    redirect('/dashboard/invoices');
+  } catch (error) {
+    return { message: "Database Error: Failed to Update Invoice." };
+  }
+  revalidatePath("/dashboard/invoices");
+  redirect("/dashboard/invoices");
 }
 
 export async function deleteInvoice(id: string) {
-    throw new Error('Failed to Delete Invoice');
-
-    try {
-        await sql`DELETE FROM invoices WHERE id = ${id}`;
-        revalidatePath('/dashboard/invoices');
-        return { message: 'Deleted Invoice.' };
-    } catch (error) {
-        return { message: 'Database Error: Failed to Delete Invoice.' };
-    }
+  // throw new Error('Failed to Delete Invoice');
+  try {
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+    revalidatePath("/dashboard/invoices");
+    return { message: "Deleted Invoice." };
+  } catch (error) {
+    return { message: "Database Error: Failed to Delete Invoice." };
+  }
 }
