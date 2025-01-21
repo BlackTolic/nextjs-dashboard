d0ee4351953563e9e0d8a00d068d108387d59030
月牙魔法师 <617938514@qq.com>
2025-01-22 00:16:46 +0800

    安装nextui

--- app/dashboard/subscriptions/page.tsx
+++ app/dashboard/subscriptions/page.tsx
@@ -1,11 +1,12 @@
 'use client';
 
-import { lusitana } from "@/app/ui/fonts";
-import { BellIcon } from "@heroicons/react/24/outline";
-import SubscriptionCard from "@/app/ui/common/subscription-card";
-import { subscriptions } from "@/app/ui/common/constant";
-import { Button } from "@/app/ui/button";
+import { lusitana } from '@/app/ui/fonts';
+import { BellIcon } from '@heroicons/react/24/outline';
+import SubscriptionCard from '@/app/ui/components/card/subscription-card';
+import { subscriptions } from '@/app/ui/components/card/constant';
+import { Button } from '@/app/ui/button';
 import { updateStocksFromXueqiu } from '@/app/lib/db/stock/stock-list';
+import SeniorTable from '@/app/ui/components/senior-table';
 
 const Subscriptions = () => {
   const handleTest = async () => {
@@ -29,6 +30,7 @@ const Subscriptions = () => {
 
   return (
     <div>
+      <SeniorTable />
       <div className="flex w-full items-center justify-between">
         <h1 className={`${lusitana.className} text-2xl flex items-center gap-2`}>
           <BellIcon className="w-6 h-6" />
@@ -51,8 +53,18 @@ const Subscriptions = () => {
             className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
           />
           <div className="absolute inset-y-0 right-0 flex items-center pr-3">
-            <svg className="h-5 w-5 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+            <svg
+              className="h-5 w-5 text-gray-400"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              />
             </svg>
           </div>
         </div>
@@ -72,8 +84,8 @@ const Subscriptions = () => {
               selectedOptions: {
                 showMaxValue: true,
                 showMinValue: true,
-                showChart: false
-              }
+                showChart: false,
+              },
             }}
           />
         ))}
