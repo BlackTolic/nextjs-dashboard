dc735534c026fd3a0ca1d5de8ebdf6b8932357f3
月牙魔法师 <617938514@qq.com>
2025-02-09 19:50:06 +0800

    添加订阅保存功能

---
+++ app/stockPriceMonitoring/mysql.js
@@ -0,0 +1,46 @@
+const mysql = require('mysql');
+const { dbConfig } = require('../utils/monitorStock/config.js');
+
+const connection = mysql.createConnection(dbConfig);
+
+function connectDatabase() {
+  return new Promise(resolve => {
+    connection.connect(error => {
+      if (error) throw error;
+      console.log('成功连接数据库！');
+      resolve('成功连接数据库！');
+    });
+  });
+}
+function closeDatabaseConnect() {
+  return new Promise(resolve => {
+    connection.end(err => {
+      if (err) throw err;
+      console.log('数据库连接已关闭');
+      resolve('数据库连接已关闭');
+    });
+  });
+}
+function mysqlQuery(sqlStr) {
+  return new Promise(resolve => {
+    connection.query(sqlStr, (error, results) => {
+      if (error) throw error;
+      resolve(results);
+    });
+  });
+}
+
+async function getMessage() {
+  const sqlStr = "select * from t_message where isShow = 1 and isActive = 1 and type like '股票-%';";
+  const res = await mysqlQuery(sqlStr);
+  return [...res];
+}
+
+async function getTableDate() {
+  await connectDatabase();
+  const res = await getMessage();
+  await closeDatabaseConnect();
+  return res;
+}
+
+module.exports = getTableDate;
---
+++ app/utils/notification-tool/email.ts
@@ -0,0 +1,43 @@
+'use server';
+import nodemailer, { TransportOptions } from 'nodemailer';
+
+const mail = {
+  host: 'smtp.qq.com', // 主机
+  secureConnection: true, // 使用 SSL
+  port: 465, // SMTP 端口
+  auth: {
+    user: '617938514@qq.com', // 自己用于发送邮件的账号
+    pass: 'wubwzddzmunjbbdg' // 授权码(改成自己账号对应即可,获取方法: QQ邮箱-->设置-->账户-->POP3/IMAP/SMTP/Exchange/CardDAV/CalDAV服务-->IMAP/SMTP开启 复制授权码)
+  }
+};
+
+// 邮件选项
+export interface MailOptions {
+  to: string;
+  subject: string;
+  text?: string;
+  html?: string;
+}
+
+// 创建SMTP传输
+const smtpTransport = nodemailer.createTransport(mail);
+
+// 发送邮件
+export const sendMail = (options: MailOptions) => {
+  return new Promise(resolve => {
+    const mailOptions = {
+      from: mail.auth.user,
+      ...options
+    };
+    // 发送邮件
+    smtpTransport.sendMail(mailOptions, function (error, response) {
+      if (error) {
+        console.error('发送邮件失败：', error);
+      } else {
+        console.log('邮件发送成功');
+      }
+      smtpTransport.close(); // 发送完成关闭连接池
+      resolve(true);
+    });
+  });
+};
