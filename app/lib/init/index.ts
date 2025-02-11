89443fe7c13671ba8af7794d1f1e48c45f18b184
月牙魔法师 <617938514@qq.com>
2025-02-11 21:06:01 +0800

    邮件开发1

--- app/lib/init/index.ts
+++ app/lib/init/index.ts
@@ -1,9 +1,10 @@
 import { startTimeLogger } from './scheduler';
+import { postMail } from '../utils/monitor-stock/index';
 
 export function initializeApp() {
   console.log('应用初始化');
   // 启动定时任务
-  startTimeLogger();
+  startTimeLogger(() => postMail([], []));
   // 其他初始化代码...
 }
 
