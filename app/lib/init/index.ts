bfd37a6fef2ea91dfeac49768f887ac9ae55fc40
月牙魔法师 <617938514@qq.com>
2025-02-17 23:17:31 +0800

    代码暂存3

--- app/lib/init/index.ts
+++ app/lib/init/index.ts
@@ -4,7 +4,7 @@ import { postMail } from '../utils/monitor-stock/index';
 export function initializeApp() {
   console.log('应用初始化');
   // 启动定时任务
-  startTimeLogger(() => postMail([], []));
+  // startTimeLogger(() => postMail([], []));
   // 其他初始化代码...
 }
 
